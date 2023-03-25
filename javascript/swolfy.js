265eae5de21ec0d24fe5ee3dce8daca209553c35
41Swolfy <41Swolfy21@gmail.com>
2023-03-26 00:47:56 +0100

    update

--- javascript/swolfy.js
+++ javascript/swolfy.js
@@ -116,13 +116,13 @@ const editions = ["2052", "1332", "1959", "1998", "269", "509", "123", "79", "19
 
 async function getdata() {
   const currentDate = new Date();
-  const next70Days = new Date();
-  next70Days.setDate(currentDate.getDate() + 70);
+  const next67Days = new Date();
+  next67Days.setDate(currentDate.getDate() + 67);
 
   const data = await fetchData(editions);
   for (let i = 0; i < data.length; i++) {
     const Mangadate = new Date(data[i].date);
-    if (Mangadate > currentDate && Mangadate <= next70Days) {
+    if (Mangadate > currentDate && Mangadate <= next67Days) {
       let Band = data[i].numberDisplay;
       const Mangadates = new Date(data[i].date);
       const priceInCent = data[i].price;
