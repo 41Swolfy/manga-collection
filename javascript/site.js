d03e4a1d2aab95c6ced33e71ae1a6e60e9c191bf
Swolfy <41Swolfy21@gmail.com>
2023-08-30 05:01:40 +0200

    fix

--- javascript/site.js
+++ javascript/site.js
@@ -3,7 +3,76 @@ var imgname;// img name ==  image for manga
 
 
 //missing manga  Artbooks :Black Butler;
+function createDropdownMenu() {
 
+    const links = [
+        { href: `/Ongoing/O_af.html`, text: "A-F" },
+        { href: `/Ongoing/O_gl.html`, text: "G-L" },
+        { href: `/Ongoing/O_mq.html`, text: "M-Q" },
+        { href: `/Ongoing/O_rs.html`, text: "R-S" },
+        { href: `/Ongoing/O_tz.html`, text: "T-Z" },
+    ];
+
+    if (i === 2) {
+        links.forEach((link, index) => {
+            link.href = link.href.replace("Ongoing", "Completed");
+            link.href = link.href.replace("/O_af", "/C_af");
+            link.href = link.href.replace("/O_gl", "/C_gl");
+            link.href = link.href.replace("/O_mq", "/C_mq");
+            link.href = link.href.replace("/O_rs", "/C_rt");
+            link.href = link.href.replace("/O_tz", "/C_uz");
+
+            if (index === 3) {
+                link.text = "R-T";
+            } else if (index === 4) {
+                link.text = "U-Z";
+            }
+        });
+    } else if (i === 3) {
+        links.forEach((link) => {
+            link.href = link.href.replace("Ongoing", "Dropped");
+            link.href = link.href.replace("/O_af", "/D_af");
+            link.href = link.href.replace("/O_gl", "/D_gl");
+            link.href = link.href.replace("/O_mq", "/D_mq");
+            link.href = link.href.replace("/O_rs", "/D_rt");
+            link.href = link.href.replace("/O_tz", "/D_uz");
+        });
+    }
+
+    // create links and append to dropdown content
+    links.forEach((link) => {
+        const a = document.createElement("a");
+        a.setAttribute("href", link.href);
+        a.textContent = link.text;
+        dropdownContent.appendChild(a);
+    });
+
+    // easter egg
+    if (i === 1) {
+        const easterEgg = document.createElement("a");
+        easterEgg.setAttribute("href", "/swolfy.html");
+
+        dropdownContent.appendChild(easterEgg);
+    }
+    if (i === 2) {
+        const easterEgg2 = document.createElement("a");
+        easterEgg2.setAttribute("href", "/past.html");
+
+        dropdownContent.appendChild(easterEgg2);
+    }
+
+    // append button and dropdown content to dropdown
+    dropdown.appendChild(button);
+    dropdown.appendChild(dropdownContent);
+
+    // append dropdown to body
+    document.body.appendChild(dropdown);
+}
+
+
+
+
+createDropdownMenu();
 
 function buildbody() {
 
